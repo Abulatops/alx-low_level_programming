e8d55c8db69428efd3457c980251a78d39447c6e
Abulatops <temitopeabayomi424@gmail.com>
2023-08-19 14:35:58 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,7 +15,9 @@ if (i % 3 == 0 && i % 5 != 0)
 {
 printf(" Fizz");
 }
-else if (i % 5 == 0 && i % 3 != 0}                       printf(" Buzz");
+else if (i % 5 == 0 && i % 3 != 0)
+{
+printf(" Buzz");
 }
 else if (i % 3 == 0 && i % 5 == 0)
 {
@@ -29,7 +31,6 @@ else
 {
 printf(" %d", i);
 }
-else
 }
 printf("\n");
 return (0);
