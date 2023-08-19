eac7b24eb4d381d67135a089b438253739aaa352
Abulatops <temitopeabayomi424@gmail.com>
2023-08-19 14:27:49 -0700

    triangles

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,7 +9,6 @@ void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
-int  fizz_buzz.c(int c);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
