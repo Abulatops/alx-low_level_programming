2fcacbac801384b29ee222fae40599517cb4eb24
Abulatops <temitopeabayomi424@gmail.com>
2023-09-27 20:16:53 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,7 +8,7 @@
 */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-listint_t *tem = *head;
+listint_t *temp = *head;
 listint_t *current = NULL;
 unsigned int i = 0;
 if (*head == NULL)
@@ -16,18 +16,18 @@ return (-1);
 if (index == 0)
 {
 *head = (*head)->next;
-free(tem);
+free(temp);
 return (1);
 }
 while (i < index - 1)
 {
 if (!temp || !(temp->next))
 return (-1);
-tem = tem->next;
+temp = temp->next;
 i++;
 }
-current = tem->next;
-tem->next = current->next;
+current = temp->next;
+temp->next = current->next;
 free(current);
 return (1);
 }
