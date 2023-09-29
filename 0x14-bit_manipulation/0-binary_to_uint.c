37a56953dd87a40e161a6fdd30eab9b61f71acb8
Abulatops <temitopeabayomi424@gmail.com>
2023-09-29 16:30:58 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,10 +7,10 @@
 */
 unsigned int binary_to_uint(const char *b)
 {
-if (!b)
-	return (0);
-unsigned int decimal = 0;
 int i = 0;
+unsigned int decimal = 0;
+if (!b)
+return 0;
 for (i = 0; b[i]; i++)
 {
 if (b[i] < '0' || b[i] > '1')
