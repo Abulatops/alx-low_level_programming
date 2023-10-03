c5130f92a18fcb4c1ccc70e9b02809c9b1a14182
Abulatops <temitopeabayomi424@gmail.com>
2023-10-02 18:03:41 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,20 +11,16 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 char *buffer;
-ssize_t fd
+ssize_t fd;
 ssize_t bytesWritten;
 ssize_t bytesRead;
 fd = open(filename, O_RDONLY);
 if (fd == -1)
 return (0);
 buffer = malloc(sizeof(char) * letters);
-bytesWritten = read(fd, buffer, letters);
-bytesRead = write(STDOUT_FILENO, buffer, bytesRead);
+bytesRead = read(fd, buffer, letters);
+bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
 free(buffer);
 close(fd);
 return(bytesWritten);
-
-}
-
-
 }
