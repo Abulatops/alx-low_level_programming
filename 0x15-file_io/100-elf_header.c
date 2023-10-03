7de7d234dce3b206e87dfe8e704285410811e6d3
Abulatops <temitopeabayomi424@gmail.com>
2023-10-02 19:43:27 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -229,7 +229,7 @@ void close_elf(int elf)
 {
 if (close(elf) == -1)
 {
-dprintf(STDERR_FILENO,"Error: Can't close fd %d\n", elf);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", elf);
 exit(98);
 }
 }
